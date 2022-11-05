535bd85e023484bc74ece73b0c0460a2d6cbc618
chepyego <jchepyego@gmail.com>
2022-11-06 00:41:54 +0300

    add checkbox and modified the style

--- main.js
+++ main.js
@@ -1,78 +1,46 @@
-// alert("jemimah");
 
-let selectButton = document.getElementById('input-icon');
+function showInputValue(todo){
+    let li = document.createElement('li');
+    let liItems = document.getElementById('list-items')
+    li.innerHTML = `<input type="checkbox" class="check-box"></input>
+                     <span class="list-items">${todo}</span>
+                    <button class="edit-button b-buttons">edit</button>
+                     <button class="delete-button b-buttons">delete</button>`
 
-// let boxOne = document.querySelector('#box-1');
-// let boxTwo = document.querySelector('#box-2');
-// let boxThree = document.querySelector('#box-3');
-// let pE = document.querySelector('#para-body')
-
-
-
-function showInput(todo){
-    let liE = document.createElement('li');
-    let contentBody = document.getElementById('content');
-    // let inputButton = document.getElementById('input');
-    
-    
-    // let inputValue = inputButton.value;
-
-    liE.innerHTML = ` <span class="list-items">${todo}</span>
-    <div class="modify-buttons">
-        <button id="edit-button" class="edit-button-actions">edit</button>
-        <button id="delete-button" class="delete-button-actions">delete</button>
-    </div>`
-    liE.classList.add('todo-content-items')
-    contentBody.appendChild(liE)
-
+    li.classList.add('todo-items-child')                 
+   liItems.appendChild(li);
+   
 
 }
 
-function submitForm(e){
+function TodoInput(e){
+
     e.preventDefault();
+
     let inputButton = document.getElementById('input');
-    
     let inputValue = inputButton.value;
-
-    showInput(inputValue);
+     
+    inputButton = " "
+    showInputValue(inputValue);
 
 }
 
-const formInput = document.querySelector('#form');
 
-formInput.addEventListener('submit', submitForm)
+let submitForm = document.getElementById('form');
 
-function deleteElement(){
-    console.log(deleteButton);
-    let deleteElements = document.getElementsByClassName('list-items')
-    deleteElements = deleteElements.length -1
+submitForm.addEventListener('click', TodoInput);
+
+// delete and edit functions
+
+function deleteElements(){
+  let deleteList = document.getElementsByClassName('list-items');
+  let removedElement = (deleteList.length -1).pop;
+
+  console.log(removedElement);
+
+
 }
-    let deleteButton = document.querySelector('#delete-button');
-    deleteButton.addEventListener('click', deleteElement);
 
+let deleteButton = document.getElementById('delete-button');
 
-// document.addEventListener('click' ,function(){
-//     console.log("hurray");
-//     boxOne.style.backgroundColor = "yellow";
-//     boxTwo.style.backgroundColor = "purple";
-//     boxThree.style.backgroundColor = "black";
-
-//     let newP = document.createElement('p');
-//     newP.innerText = "Jemimah is learning javascript";
-//     newP.style.position = "absolute";
-//     newP.style.backgroundColor = "pink";
-//     pE.appendChild(newP);
-
-// });
-
-
-// document.addEventListener('click', inputSubmitForm(){
-//     // console.log("trial")
-//     let li = document.createElement('li');
-//     li.innerText = inputButton.value;
-//     // paragraph.classList.add('para-style') ;
-//     contentBody.appendChild(liElement);
-//     inputButton.value = ""
-
-// });
-
+deleteButton.addEventListener('click', deleteElements);
